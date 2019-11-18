35401267dd331fdf3c9c349d19cabb35bfb05edb
terminalforlife <terminalforlife@yahoo.com>
2019-11-18 16:11:20 +0000

    Remove absolute executable paths

--- update_links.sh
+++ update_links.sh
@@ -3,37 +3,39 @@
 #----------------------------------------------------------------------------------
 # Project Name      - bashconfig/update_links.sh
 # Started On        - Sun 22 Oct 00:15:02 BST 2017
-# Last Change       - Thu  9 May 13:43:28 BST 2019
+# Last Change       - Mon 18 Nov 16:10:36 GMT 2019
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #----------------------------------------------------------------------------------
 # Just a simple, quick script to update the hard links when changing branches.
 #------------------------------------------------------------------------------MAIN
 
-XERR(){ printf "[L%0.4d] ERROR: %s\n" "$1" "$2" 1>&2; exit 1; }
-ERR(){ printf "[L%0.4d] ERROR: %s\n" "$1" "$2" 1>&2; }
+FAIL(){
+	printf "[L%0.4d] ERROR: %s\n" "$2" "$3" 1>&2
+	[ $1 -eq 1 ] && exit 1
+}
 
 declare -i DEPCOUNT=0
-for DEP in /bin/{ln,rm}; {
-	[ -x "$DEP" ] || {
-		ERR "$LINENO" "Dependency '$DEP' not met."
+for DEP in rm ln; {
+	if ! type -fP "$DEP" > /dev/null 2>&1; then
+		FAIL 0 "$LINENO" "Dependency '$DEP' not met."
 		DEPCOUNT+=1
-	}
+	fi
 }
 
 [ $DEPCOUNT -eq 0 ] || exit 1
 
-[ "${PWD//*\/}" == "BashConfig" ] || XERR "Not in the repository's root directory."
+[ "${PWD//*\/}" == "BashConfig" ] || FAIL 1 "Not in the repository's root directory."
 
 for FILE in .bashrc .bash_aliases .bash_functions .bash_logout .profile; {
-	/bin/rm -v $HOME/$FILE 2>&-
-	/bin/ln -v $FILE $HOME/$FILE 2>&-
+	rm -v $HOME/$FILE 2>&-
+	ln -v $FILE $HOME/$FILE 2>&-
 }
 
 for FILE in .shplugs/*; {
-	/bin/rm -v $HOME/$FILE 2>&-
-	/bin/ln -v $FILE $HOME/$FILE 2>&-
+	rm -v $HOME/$FILE 2>&-
+	ln -v $FILE $HOME/$FILE 2>&-
 }
 
-/bin/rm -v $HOME/.inputrc 2>&-
-/bin/ln -v .inputrc $HOME/.inputrc 2>&-
+rm -v $HOME/.inputrc 2>&-
+ln -v .inputrc $HOME/.inputrc 2>&-
