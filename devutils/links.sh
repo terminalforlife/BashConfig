a256bad8f2852ce7cfd5e3256846008a4170d8e6
terminalforlife <terminalforlife@yahoo.com>
2020-01-28 02:56:17 +0000

    Update `links.sh`

--- devutils/links.sh
+++ devutils/links.sh
@@ -3,20 +3,26 @@
 #----------------------------------------------------------------------------------
 # Project Name      - BashConfig/devutils/links.sh
 # Started On        - Sun 22 Oct 00:15:02 BST 2017
-# Last Change       - Thu  9 Jan 18:44:46 GMT 2020
+# Last Change       - Tue 28 Jan 02:42:31 GMT 2020
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #----------------------------------------------------------------------------------
 # Just a simple, quick script to update the hard links when changing branches.
 #----------------------------------------------------------------------------------
 
+set -e
 . /usr/lib/tflbp-sh/ChkDep
+set +e
 
 ChkDep rm ln
 
 cd "$HOME/GitHub/terminalforlife/Personal/BashConfig/source"
 
-for CurFile in .inputrc .yashrc .bashrc .bash_logout .bash_aliases .bash_functions; do
+for CurFile in\
+\
+	.inputrc .yashrc .profile .bash_logout\
+	.bash_aliases .bash_functions .bashrc
+do
 	rm -v "$HOME/$CurFile" 2> /dev/null
 	ln -v "$CurFile" "$HOME/$CurFile" 2> /dev/null
 done
