61ef53034df7d0e49db85d13f861eb40c198aacf
terminalforlife <terminalforlife@yahoo.com>
2020-11-11 16:59:17 +0000

    Remove libtflbp-sh from links.sh & robustify
    There's now a safety check before `rm` and `ln` operation.

--- devutils/links.sh
+++ devutils/links.sh
@@ -3,26 +3,22 @@
 #------------------------------------------------------------------------------
 # Project Name      - BashConfig/devutils/links.sh
 # Started On        - Sun 22 Oct 00:15:02 BST 2017
-# Last Change       - Fri 31 Jan 21:55:37 GMT 2020
+# Last Change       - Wed 11 Nov 16:58:36 GMT 2020
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #------------------------------------------------------------------------------
 # Just a simple, quick script to update the hard links when changing branches.
 #------------------------------------------------------------------------------
 
-set -e
-. /usr/lib/tflbp-sh/ChkDep
-set +e
-
-ChkDep rm ln
-
-cd "$HOME/GitHub/terminalforlife/Personal/BashConfig/source"
-
-for CurFile in\
-\
-	.inputrc .yashrc .profile .bash_logout\
-	.bash_aliases .bash_functions .bashrc
-do
-	rm -v "$HOME/$CurFile" 2> /dev/null
-	ln -v "$CurFile" "$HOME/$CurFile" 2> /dev/null
-done
+{
+	if cd "$HOME/GitHub/terminalforlife/Personal/BashConfig/source"; then
+		for CurFile in\
+		\
+			.inputrc .yashrc .profile .bash_logout\
+			.bash_aliases .bash_functions .bashrc
+		do
+			rm -v "$HOME/$CurFile"
+			ln -v "$CurFile" "$HOME/$CurFile"
+		done
+	fi
+} 2> /dev/null
